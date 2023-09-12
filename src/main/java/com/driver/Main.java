8f31950b54d7e7232534cef756134540e0cce8fc
Your Name <you@example.com>
2023-09-12 13:54:24 +0530

    third attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,8 +5,20 @@ public class Main {
               B obj = new B();
               String ans = obj.meth();
 
-              B b1 = new B();
-              String ans1 = b1.meth();
-            //  System.out.println(ans + " " + ans1);
+//              B b1 = new B();
+//              String ans1 = b1.meth();
+              System.out.println(ans);
+       }
+
+       public static class A{
+              public String meth(){
+                     return "Invoking method from class A";
+              }
+       }
+       public static class B extends A{
+             @Override
+             public String meth(){
+                    return "Method is overridden in Extendend class B";
+             }
        }
 }
